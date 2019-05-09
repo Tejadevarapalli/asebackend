32be0b77086296f353324975f32a25e1775954e3
Devarapalli Teja <41124424+Tejadevarapalli@users.noreply.github.com>
2019-05-09 14:41:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', 'https://ase-frontend.herokuapp.com');
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
